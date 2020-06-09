f628e92c2b1a95ce3645a513255073db5d53dae2
thallosaurus <activator112233@gmail.com>
2020-06-09 19:00:25 +0200

    implemented a pagechanger

--- main.js
+++ main.js
@@ -1,45 +1,39 @@
-let title = "DJ PRiSMFLUX";
-let titleIndex = 0;
-let titleElement;
-let titleDrawInterval;
+const LOADINGERROR = `# Oh-uh!
+Looks like there was an Error while catching the content.
 
-let undertitle = "best techno outta steinwald";
-let undertitleIndex = 0;
-let undertitleElement;
-let undertitleDrawInterval;
+Found a bug or noticed something strange? Report it [here](https://github.com/thallosaurus/thallosaurus.github.io/issues)`;
+
+const urlParams = new URL(location.href).searchParams;
+
+const page = urlParams.get("p") || "index";
 
 let navbar;
-
-const urlParams = new URLSearchParams(window.location);
 
 let drawSpeed = 25;
 let howLongAfterWriteFinishShouldWriterStay = 500;
 
-window.onload = function()
-{
+window.onload = function () {
     this.fetch("elements.json")
-    .then((data) => {
-        return data.json();
-    }).then((json) => {
-        asyncCreateFromObject(json);
-    }).catch(e => {
-        console.log(e);
-    });
+        .then((data) => {
+            return data.json();
+        }).then((json) => {
+            asyncCreateFromObject(json);
+        }).catch(e => {
+            console.log(e);
+        });
 
-    this.insertContent();
+    this.insertContent(page);
+    this.unhideText();
 }
 
-function showPage()
-{
+function showPage() {
     document.getElementById("hider").className = "show";
 }
 
-async function asyncCreateFromObject(jsonMap)
-{
+async function asyncCreateFromObject(jsonMap) {
     //append elements before they get typed out, so the page doesnt jump around
     jsonMap.map(e => {
         let f = document.createElement(e.element);
-        //e.id = obj.id;
         Object.assign(f, e);
         f.textContent = "";
 
@@ -47,16 +41,14 @@ async function asyncCreateFromObject(jsonMap)
         e.__element = f;
     });
 
-    setTimeout(showPage, 10);
+    setTimeout(showPage, 50);
 
-    for (let i = 0; i < jsonMap.length; i++)
-    {
+    for (let i = 0; i < jsonMap.length; i++) {
         await this.animObject(jsonMap[i]);
     }
 }
 
-function animObject(obj)
-{
+function animObject(obj) {
     return new Promise((res, rej) => {
         let text = obj.text;
         let index = 0;
@@ -64,13 +56,11 @@ function animObject(obj)
         //clear content
 
         let r = setInterval(() => {
-            if (index != text.length)
-            {
+            if (index != text.length) {
                 obj.__element.dataset.animstate = "drawing";
                 obj.__element.textContent += text[index++];
             }
-            else
-            {
+            else {
                 obj.__element.dataset.animstate = "finished";
                 setTimeout(() => {
                     res();
@@ -81,13 +71,26 @@ function animObject(obj)
     });
 }
 
-function insertContent()
-{
-    let charsReceived = 0;
-    let buffer = "";
-    fetch("test.md").then((data) => {
-        data.text().then((t) => {
-            document.getElementById("text_container").innerHTML = marked(t);
-        });
+function insertContent(page) {
+    fetch(`md/${page}.md`).then((data) => {
+        if (data.ok) {
+            data.text().then((t) => {
+                writeToContent(marked(t));
+            });
+        }
+        else {
+            throw data;
+        }
+    }).catch((e) => {
+        writeToContent(marked(LOADINGERROR + `\n\n(${e.status} - ${e.statusText})`));
     });
+}
+
+function unhideText() {
+    document.getElementById("text_container").style.opacity = "1";
+}
+
+function writeToContent(text)
+{
+    document.getElementById("text_container").innerHTML = `<div class="textContent">${text}</div>`;
 }
