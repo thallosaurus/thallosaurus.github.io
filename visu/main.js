3b356cc8fa1ff0232ad150b91c0ddc378a99df50
thallosaurus <activator112233@gmail.com>
2020-11-04 08:08:36 +0100

    Added debug adapter for visualizer - remotejs.com

--- visu/main.js
+++ visu/main.js
@@ -1,19 +1,33 @@
 let content, audioCtx, analyser, dataArray, htmlElements;
 
 const height = 20;
-const width = 64;
-const DEFAULT_GLYPH = "A";
+const width = 128;
+const DEFAULT_GLYPH = "0";
 
 window.onload = () => {
+  if (getParam("dbg")) {
+    createDebugAdapter(getParam("dbg"));
+  }
   content = document.querySelector("#content");
   htmlElements = [];
   createBackground(width, height, content);
 }
 
 function createBackground(fnWidth, fnHeight, fnWindow = null) {
-  if(!fnWindow) throw new Error("You didn't specify an element");
+  if (!fnWindow) throw new Error("You didn't specify an element");
 
-  for (let lY = 0; lY < fnHeight; lY++) {
+/*   for (let lY = 0; lY < fnHeight; lY++) {
+    let row = document.createElement("div");
+    row.dataset.rowNumber = lY
+    for (let lX = 0; lX < fnWidth; lX++) {
+      let col = document.createElement("span");
+      col.innerText = DEFAULT_GLYPH;
+      col.dataset.colNumber = lX;
+      row.append(col);
+    }
+    fnWindow.append(row);
+  } */
+  for (let lY = fnHeight - 1; lY > -1; lY--) {
     let row = document.createElement("div");
     row.dataset.rowNumber = lY
     for (let lX = 0; lX < fnWidth; lX++) {
@@ -33,7 +47,7 @@ function getXY(x, y) {
 
 // AudioContext Initialization and all other stuff you need to process Audio Data
 function initAudioContext() {
-  if (!"AudioContext" in window || !AudioContext) throw new Error("Failed to catch AudioContext - Not supported");
+  if (!"AudioContext" in window || !AudioContext) {alert("AudioContext not supported"); throw new Error("Failed to catch AudioContext - Not supported")};
 
   audioCtx = new AudioContext();
   analyser = audioCtx.createAnalyser();
@@ -43,11 +57,15 @@ function initAudioContext() {
 }
 
 function initMicrophone(fnCtx) {
-  navigator.getUserMedia({ audio: true }, (sound) => {
-    let cbStream = fnCtx.createMediaStreamSource(sound);
-    cbStream.connect(analyser);
-    capture(analyser, dataArray);
-  }, (error) => {throw error});
+  try {
+    navigator.getUserMedia({ audio: true }, (sound) => {
+      let cbStream = fnCtx.createMediaStreamSource(sound);
+      cbStream.connect(analyser);
+      capture(analyser, dataArray);
+    }, (error) => { alert(error); throw error });
+  } catch (e) {
+    alert(e.message);
+  }
 }
 
 function capture(fnAnalyser, array) {
@@ -70,13 +88,13 @@ function capture(fnAnalyser, array) {
       } else {
         fg.className = "black";
       }
-      
+
       if (k == h && k != 0) {
         fg.style.opacity = opacity;
       }
     }
   }
-  requestAnimationFrame(()=> {capture(fnAnalyser, dataArray)});
+  requestAnimationFrame(() => { capture(fnAnalyser, dataArray) });
 }
 
 function map(value, low1, high1, low2, high2) {
@@ -86,9 +104,21 @@ function map(value, low1, high1, low2, high2) {
 function reinit() {
   for (let e of content.children) {
     for (let f of e.children) {
-      f.innerText = DEFAULT_GLYPH;
+      // f.innerText = DEFAULT_GLYPH;
       f.className = "";
       f.style.opacity = 1;
     }
   }
+}
+
+function getParam(param) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
+}
+
+function createDebugAdapter(id) {
+  let dbg = document.createElement("script");
+  dbg.dataset.consolejsChannel = id;
+  dbg.src = "https://remotejs.com/agent/agent.js";
+  document.head.append(dbg);
 }
